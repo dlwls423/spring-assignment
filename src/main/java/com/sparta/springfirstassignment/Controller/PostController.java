8c01f7ff24eebd29d1e23411befe1a0b192f0c64
LeeYejin <leeyejin113@gmail.com>
2023-11-03 20:10:11 +0900

    Refactor: 코드 정리

--- src/main/java/com/sparta/springfirstassignment/Controller/PostController.java
+++ src/main/java/com/sparta/springfirstassignment/Controller/PostController.java
@@ -1,7 +1,6 @@
 package com.sparta.springfirstassignment.Controller;
 
 
-import com.sparta.springfirstassignment.IncorrectPwException;
 import com.sparta.springfirstassignment.Service.PostService;
 import com.sparta.springfirstassignment.dto.PostRequestDto;
 import com.sparta.springfirstassignment.dto.PostResponseDto;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
